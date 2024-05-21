ca56b4869cd8622392c55ebce0be460a6f953cc6
Evgeny <transingularity@gmail.com>
2024-05-21 16:32:21 +0300

    chore: fixed deps

--- library-backend/src/main/java/ru/dankoy/library/LibraryBackendApplication.java
+++ library-backend/src/main/java/ru/dankoy/library/LibraryBackendApplication.java
@@ -4,11 +4,12 @@ import com.github.cloudyrock.spring.v5.EnableMongock;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
-@EnableEurekaClient
+
+@EnableDiscoveryClient
 @EnableMongock
 @EnableMongoRepositories(basePackages = {"ru.dankoy.library.core.repository"})
 @EnableEncryptableProperties
